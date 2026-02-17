728902d0e00b7e5c531cf5839b0c9e1c522b663f
hilaire <hilaire@gnu.org>
2026-02-17 21:24:31 +0100

    DrGeo translation split in two catalogs

--- i18n/update-po.sh
+++ i18n/update-po.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# OBSOLETE, done by the Launchpad service
+catalogues="drgeo drgeocore"
+languages="ar ast ca ca@valencia cs da de en_GB eo es fi fr hy ia it ja ko lt nl pl pt pt_BR ro ru sk sv ta te th tr ug uk uz vi zh_CN zh_TW"
 
 cd po
-
-lang="ar ast ca ca@valencia cs da de en_GB eo es fi fr hy ia it ja ko lt nl pl pt pt_BR ro ru sk sv ta te th tr ug uk uz vi zh_CN zh_TW"
-
-for l in $lang; do
-    msgmerge -U $l.po drgeo.pot
-#    msgmerge -U $l-cuis.po cuis.pot
-done
-
-cd -
+for cat in $catalogues; do
+    cd $cat
+    for l in $languages; do
+	msgmerge -U $l.po $cat.pot
+    done
+    cd ..
+done	
+cd ..
